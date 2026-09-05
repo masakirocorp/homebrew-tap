c2ba84ac92a73f2dcb4427fd0bdd9f66a73ca362
charlie <zugasticharlie@gmail.com>
2026-09-05 00:32:53 -0600

    chore: update ghui to 0.10.0-masakiro.6

--- Formula/ghui.rb
+++ Formula/ghui.rb
@@ -1,30 +1,30 @@
 class Ghui < Formula
   desc "Terminal UI for GitHub pull requests"
   homepage "https://github.com/masakirocorp/ghui"
-  version "0.10.0-masakiro.5"
+  version "0.10.0-masakiro.6"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/masakirocorp/ghui/releases/download/v#{version}/ghui-darwin-arm64.tar.gz"
-      sha256 "8b78d851d006452115676f62ba58acb7ffc3e12861204256f3ac610e180a3e5a"
+      sha256 "7197ea5a6efbbc251939ddc63dbf3596c107ebc4edd528ed209fca5b7e3422ed"
     end
 
     on_intel do
       url "https://github.com/masakirocorp/ghui/releases/download/v#{version}/ghui-darwin-x64.tar.gz"
-      sha256 "e7ed0bb0727cb903d08ec5f9567f87a241577254b04cef5081eaaf0d0f605337"
+      sha256 "32f392ab7b18aedce3092f1f92e1143d9a4c15950bdbda279982a94f20ef0e40"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/masakirocorp/ghui/releases/download/v#{version}/ghui-linux-arm64.tar.gz"
-      sha256 "5308ba197e7a0efbd8706f1baf3d2f177aa0bde22d3b4426dc242ddc9a7405bc"
+      sha256 "6bcce77e63301e9963c61a74490c2e6f39c0bb3935eba697d35d32bc14881be3"
     end
 
     on_intel do
       url "https://github.com/masakirocorp/ghui/releases/download/v#{version}/ghui-linux-x64.tar.gz"
-      sha256 "de28574857d3bc80a2eadc4527b8ceb3f7a0435a97954599fda7b234d58a7169"
+      sha256 "aa15426a61543fd0e2e834e68df612e417e22f80c8314998d3397a0dc43d91a9"
     end
   end
 
